27ec8aa4992ff31aa4b2e3d8f0f2feab75d45462
Leonardo Miguel dos Santos <leoms-98@hotmail.com>
2024-06-21 18:11:45 -0300

    Update and rename Atividade de Aula 12-03 to Atividade de Aula 12-03.java

--- Atividade de Aula 12-03.java
+++ Atividade de Aula 12-03.java
@@ -1,15 +1,15 @@
-Exercício 1) Passar o código a seguir para a linguagem Java
+//Exercício 1) Passar o código a seguir para a linguagem Java
 
-Algoritmo ExEntrada
-Declarar
-nome : alfanumérico;
-Início
-Mostrar(“Qual é o seu nome?”);
-Ler (nome)
-Mostrar ( “Já sei o seu nome ! É ”, nome);
-Fim.
+//Algoritmo ExEntrada
+//Declarar
+//nome : alfanumérico;
+//Início
+//Mostrar(“Qual é o seu nome?”);
+//Ler (nome)
+//Mostrar ( “Já sei o seu nome ! É ”, nome);
+//Fim.
 
-R: import java.util.Scanner;
+//R: import java.util.Scanner;
 
 public class ExEntrada {
     public static void main(String[] args) {
@@ -24,14 +24,14 @@ public class ExEntrada {
 
 
 
-Python: 
-Nome = input(“Qual é o seu nome?”)
-print(“Já sei o seu nome ! É {}”.format(Nome))
+//Python: 
+//Nome = input(“Qual é o seu nome?”)
+//print(“Já sei o seu nome ! É {}”.format(Nome))
 
 
-Exercício 2) A loja Compre Mais está vendendo seus produtos em 5 (cinco) prestações sem juros.
-Faça um programa que receba um valor de uma compra e mostre o valor das prestações.
-R: 
+//Exercício 2) A loja Compre Mais está vendendo seus produtos em 5 (cinco) prestações sem juros.
+//Faça um programa que receba um valor de uma compra e mostre o valor das prestações.
+//R: 
 import java.util.Scanner;
 
 public class Prestacoes {
@@ -50,17 +50,17 @@ public class Prestacoes {
 
 
 
-Python:
-X = float(input("Digite o valor total da compra: ")) 
-print("O valor de R${} é dividido em 5 prestações de R${} sem juros.".format(X, float(X/5)))
+//Python:
+//X = float(input("Digite o valor total da compra: ")) 
+//print("O valor de R${} é dividido em 5 prestações de R${} sem juros.".format(X, float(X/5)))
 
 
 
 
 
-Exercício 3) Complemente o primeiro exercício, lendo o seu último nome e mostrando o primeiro e último nome.
-R: 
-import java.util.Scanner;
+//Exercício 3) Complemente o primeiro exercício, lendo o seu último nome e mostrando o primeiro e último nome.
+//R: 
+//import java.util.Scanner;
 
 public class ExEntrada {
     public static void main(String[] args) {
@@ -78,7 +78,7 @@ public class ExEntrada {
 }
 
 
-Python:
-Nome1 = input("Qual é o seu primeiro nome?") 
-Nome2 = input("E seu sobrenome?") 
-print("Já sei o seu nome ! É {} {}".format(Nome1, Nome2))
+//Python:
+//Nome1 = input("Qual é o seu primeiro nome?") 
+//Nome2 = input("E seu sobrenome?") 
+//print("Já sei o seu nome ! É {} {}".format(Nome1, Nome2))
